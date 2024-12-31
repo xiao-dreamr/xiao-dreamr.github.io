4dc353b2ebe43a36433cd375943705f7a3c6be12
xiao_dreamr <wal20080826@outlook.com>
2025-01-01 00:12:06 +0800

    fix 25-1-1

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -31,6 +31,7 @@ declare module 'vue-router/auto-routes' {
     '/posts/csharp_typeof_gettype': RouteRecordInfo<'/posts/csharp_typeof_gettype', '/posts/csharp_typeof_gettype', Record<never, never>, Record<never, never>>,
     '/posts/ECS-PR1初识': RouteRecordInfo<'/posts/ECS-PR1初识', '/posts/ECS-PR1初识', Record<never, never>, Record<never, never>>,
     '/posts/ECS-TH1初识/': RouteRecordInfo<'/posts/ECS-TH1初识/', '/posts/ECS-TH1初识', Record<never, never>, Record<never, never>>,
+    '/posts/2024年末总结/': RouteRecordInfo<'/posts/2024年末总结/', '/posts/2024年末总结', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
